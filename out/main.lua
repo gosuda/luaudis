db8c2b0a78946e466d60e830f394821bbe1ba4a6
snowmerak <snowmerak@outlook.com>
2026-01-07 23:39:43 +0900

    feat(redis): implement Redis get and set functions, remove add functionality

--- out/main.lua
+++ out/main.lua
@@ -1,17 +1,12 @@
-local __DARKLUA_BUNDLE_MODULES={cache={}}do do local function __modImpl()
-return {
-    add = function(x, y)        
-return x + y
+local __DARKLUA_BUNDLE_MODULES={cache={}}do do local function __modImpl()return {
+    get = function(key)        
+return ((redis.call("GET", key) ))
+    end,
+    set = function(key, value)
+        return redis.call("SET", key, value)
     end,
 }
 end function __DARKLUA_BUNDLE_MODULES.a()local v=__DARKLUA_BUNDLE_MODULES.cache.a if not v then v={c=__modImpl()}__DARKLUA_BUNDLE_MODULES.cache.a=v end return v.c end end end
-local add = __DARKLUA_BUNDLE_MODULES.a()
+local redis = __DARKLUA_BUNDLE_MODULES.a()
 
-local count = 0
-count =count+ 10 
-
-local msg = string.format('Count is %s', tostring(add.add(count, 100)))
-
-if count > 5 then
-    print(msg)
-end
+return redis.get("a")
